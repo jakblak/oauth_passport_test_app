5d624fb7a29ab84e34dcf4243a7d1f7407241f88
Patrick Schroeder <schroe9@gmail.com>
2015-07-12 23:48:15 -0700

    twitter API to show last tweet, uses OAuth v1

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var secrets = require('../secrets');
 var facebook = require('../services/facebook')(secrets.fbID, secrets.fbSecret);
-
+var twitter = require('../services/twitter')(secrets.twitKey, secrets.twitSecret);
 // Only allow access /users if logged in
 // router.use('/', function(req, res, next) {
 //   if(!req.user) {
@@ -10,6 +10,15 @@ var facebook = require('../services/facebook')(secrets.fbID, secrets.fbSecret);
 //   }
 //   next();
 // });
+
+router.use('/', function(req, res, next) {
+  if(req.user.twitter) {
+    twitter.getUserTimeLine(req.user.twitter.token, req.user.twitter.tokenSecret, req.user.twitter.id, function(results) {
+      req.user.twitter.lastPost = results[0].text;
+      next();
+    });
+  }
+});
 
 /* GET users listing. */
 router.get('/', function(req, res) {
